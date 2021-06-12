85b5bb2e5e71da0c721917342df72acf39a9355a
CarlosFdezJim <carlosfdezjim@gmail.com>
2021-06-12 20:04:44 +0200

    UI Login, Manú lateral y Splash Screen

---
+++ PMR_Parking/PMR_Parking/PMR_Parking/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PMR_Parking
+{
+    public partial class App : Application
+    {
+        public static MasterDetailPage MasterDet { get; set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ PMR_Parking/PMR_Parking/PMR_Parking/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using Plugin.Geolocator;
+
+namespace PMR_Parking
+{
+    [DesignTimeVisible(true)]
+    public partial class MainPage : Shell
+    {
+        //double lati;
+        //double longi;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Localizar();
+        }
+
+        private async void Localizar()
+        {
+            //var locator = CrossGeolocator.Current; //Acceso a la API
+            //locator.DesiredAccuracy = 50; //Precisión (en metros)
+            //if (locator.IsGeolocationAvailable) //Servicio existente en el dispositivo
+            //{
+            //    if (locator.IsGeolocationEnabled) //GPS activado en el dispositivo
+            //    {
+            //        if (!locator.IsListening) //Comprueba si el dispositivo escucha al servicio
+            //        {
+            //            await locator.StartListeningAsync(TimeSpan.FromSeconds(1), 5); //Inicio de la escucha
+            //        }
+            //        locator.PositionChanged += (cambio, args) =>
+            //        {
+            //            var loc = args.Position;
+            //            lon.Text = loc.Longitude.ToString();
+            //            longi = double.Parse(lon.Text);
+            //            lat.Text = loc.Latitude.ToString();
+            //            lati = double.Parse(lat.Text);
+            //        };
+            //    }
+            //}
+        }
+
+        private async void MostrarMapa(object sender, EventArgs e)
+        {
+            //MapLaunchOptions options = new MapLaunchOptions { Name = "Mi posición actual" };
+            //await Map.OpenAsync(lati, longi, options);
+        }
+    }
+}
